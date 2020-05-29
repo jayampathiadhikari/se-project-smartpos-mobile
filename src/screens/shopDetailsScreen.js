5e95156b86567f568316083cc30254fbc010bc26
AfraHussaindeen <47135592+AfraHussaindeen@users.noreply.github.com>
2020-05-29 12:45:44 +0530

    added styles to the shop details screen

--- src/screens/shopDetailsScreen.js
+++ src/screens/shopDetailsScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import {LinearGradient} from "expo-linear-gradient";
 const axios = require('axios');
 
 
@@ -32,22 +33,64 @@ export default class ShopDetailsScreen extends React.Component {
         if (Object.keys(state.shopDetails).length===0){
         return (
             <View style={{flex: 1,alignItems: 'center',justifyContent: 'center',}} >
-                  <Text style={{fontWeight:'bold',fontSize:32}}> Loading... </Text>
+                  <Text style={{fontWeight:'bold',fontSize:20}}> Loading... </Text>
             </View>);
         }
 
         return (
+            <View style={styles.root} >
+                <LinearGradient
+                    colors={["#0033ff","#6bc1ff"]}
+                    style={{height:"20%"}}
+                />
+                <View style={styles.imageContainer}>
+                    <Image style={styles.image} source={ require('../assets/logo-pos-600.png')  }/>
+                </View>
 
-            <View style={{flex: 1,alignItems: 'center',justifyContent: 'center',}} >
-                      <Text style={{fontWeight:'bold',fontSize:32}}> {state.shopDetails.shop_name.toUpperCase()} </Text>
-                      <Text style={{fontWeight:'bold',fontSize:18,marginTop:50}}>Owner name : </Text>
-                      <Text style={{fontWeight:'bold',fontSize:20,marginTop:0 , color:'#063580'}}>{state.shopDetails.owner_name}</Text>
-                      <Text style={{fontWeight:'bold',fontSize:18,marginTop:12}}>Contact Number (Owner) : </Text>
-                      <Text style={{fontWeight:'bold',fontSize:20,marginTop:0, color:'#063580'}}>{state.shopDetails.owner_cell_num}</Text>
-                      <Text style={{fontWeight:'bold',fontSize:20,marginTop:0, color:'#063580'}}>{state.shopDetails.owner_land_num}</Text>
-                      <Text style={{fontWeight:'bold',fontSize:18,marginTop:12}}>Contact Number (Shop)  : </Text>
-                      <Text style={{fontWeight:'bold',fontSize:20,marginTop:0, color:'#063580'}}>{state.shopDetails.shop_contact_num}</Text>
+                <View style={styles.textContainer} >
+                          <Text style={styles.textHeading}> {state.shopDetails.shop_name.toUpperCase()} </Text>
+                          <Text style={{...styles.textCategory,marginTop:50}}>Owner name : </Text>
+                          <Text style={{...styles.textContent,marginTop:0 }}>{state.shopDetails.owner_name}</Text>
+                          <Text style={{...styles.textCategory,marginTop:12}}>Contact Number (Owner) : </Text>
+                          <Text style={{...styles.textContent,marginTop:0}}>{state.shopDetails.owner_cell_num}</Text>
+                          <Text style={{...styles.textContent,marginTop:0}}>{state.shopDetails.owner_land_num}</Text>
+                          <Text style={{...styles.textCategory,marginTop:12}}>Contact Number (Shop)  : </Text>
+                          <Text style={{...styles.textContent,marginTop:0}}>{state.shopDetails.shop_contact_num}</Text>
+                </View>
+
             </View>
         );
   }
 }
+
+const styles = StyleSheet.create({
+    root:{
+        flex:1
+    },
+    image:{
+        width:230,
+        height:230,
+        borderRadius:70,
+        marginTop:-100
+    },
+    imageContainer:{
+        alignItems:"center"
+    },
+    textContainer:{
+        alignItems:"center"
+    },
+    textHeading :{
+        marginTop:-40,
+        fontWeight:'bold',
+        fontSize:25
+    },
+    textCategory:{
+        fontWeight:'bold',
+        fontSize:18
+    },
+    textContent:{
+        fontWeight:'bold',
+        fontSize:20,
+        color:'#063580'
+    }
+});
